1f38a5358708081dcf013fce195632ef21b87ec7
Vyacheslav Vorobyov <vjache@gmail.com>
2011-11-03 19:33:31 +0400

    Add few examples.

---
+++ src/examples/zlists_random.erl
@@ -0,0 +1,30 @@
+%% Author: vvorobyov
+%% Created: Nov 3, 2011
+%% Description: TODO: Add description to zlists_randoms
+-module(zlists_random).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([uniform_floats/0, uniform_floats/1]).
+
+%%
+%% API Functions
+%%
+
+uniform_floats() ->
+    RandFlo=random:uniform(),
+    zlists:new([RandFlo], fun()-> uniform_floats() end).
+
+uniform_floats(State0) ->
+    {Fl, State1}=random:uniform_s(State0),
+    zlists:new([Fl],fun()-> uniform_floats(State1) end).
+
+%%
+%% Local Functions
+%%
+
